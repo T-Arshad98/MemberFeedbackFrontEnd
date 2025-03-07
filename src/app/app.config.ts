a0fae650c69117f76410aee76a469447f41430c9
Taimoor Arshad <taimoor.1998@gmail.com>
2025-03-07 16:11:02 -0700

    firebase config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore'; 
+import { firebaseConfig } from '../environments/environment';
+import { provideAnalytics, getAnalytics } from '@angular/fire/analytics';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideAnalytics(() => getAnalytics()),
+  ] 
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyAgsGtaYR2_D8mh6owqj_FB0XjT1Yu7swg",
+  authDomain: "memberfeedback-2e570.firebaseapp.com",
+  projectId: "memberfeedback-2e570",
+  storageBucket: "memberfeedback-2e570.firebasestorage.app",
+  messagingSenderId: "161965636357",
+  appId: "1:161965636357:web:ff54a104c8e2643e99956c",
+  measurementId: "G-3YGG51C2L5"
+};
